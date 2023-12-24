b35cfd53c2f3e10bb073536c6e3f2ef4240e7364
electric-mike <66960109+electric-mike@users.noreply.github.com>
2023-12-24 13:21:21 -0500

    add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), react()],
   output: "server",
   server: {
     port: 3000
   },
   preview: {
     port: 3000
-  },
+  }
 });
